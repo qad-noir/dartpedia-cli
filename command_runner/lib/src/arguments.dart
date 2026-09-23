fca49c20f4ed8e5b98342c470a2f71678979e5e6
Qad Noir <qadzealous@gmail.com>
2026-09-23 15:57:03 +0100

    Define ArgResults class

--- command_runner/lib/src/arguments.dart
+++ command_runner/lib/src/arguments.dart
@@ -25,3 +25,33 @@ class Option {
     return '--$name: $help';
   }
 }
+
+class ArgResults {
+  String? command;
+  String? commandArg;
+  Map<Option, Object?> options = {};
+
+  // Returns true if the flag exists and is true.
+  bool flag(String name) {
+    for (var option in options.keys.where(
+      (option) => option.type == OptionType.flag,
+    )) {
+      if (option.name == name) {
+        return options[option] as bool;
+      }
+    }
+    return false;
+  }
+
+  bool hasOption(String name) {
+    return options.keys.any((option) => option.name == name);
+  }
+
+  ({Option option, Object? input}) getOption(String name) {
+    var mapEntry = options.entries.firstWhere(
+      (entry) => entry.key.name == name || entry.key.abbr == name,
+    );
+
+    return (option: mapEntry.key, input: mapEntry.value);
+  }
+}
