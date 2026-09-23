60caf94f8da46d31df5effa1eb62ce515b72959f
Qad Noir <qadzealous@gmail.com>
2026-09-23 11:35:37 +0100

    Handle missing article title and user input

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,4 +1,5 @@
 import 'package:cli/cli.dart' as cli;
+import 'dart:io';
 
 const version = '0.0.1';
 void main(List<String> arguments) {
@@ -6,15 +7,25 @@ void main(List<String> arguments) {
     printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
-  } else if(arguments.first == 'search'){
-    print('Search command recognized');
-  } else{
+  } else if (arguments.first == 'search') {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+  } else {
     printUsage();
   }
 }
 
-void searchWikipedia(List<String>? arguments){
-  print('searchWikipedia received arguments: $arguments');
+void searchWikipedia(List<String>? arguments) {
+  final String articleTitle;
+
+  if(arguments == null || arguments.isEmpty){
+    print('Please provide an article title');
+    articleTitle = stdin.readLineSync() ?? '';
+  }else{
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Current article title: $articleTitle');
 }
 
 void printUsage() {
