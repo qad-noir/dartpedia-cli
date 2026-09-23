3b0e5f147f84b3ce56bacf6fe8373f4b0e973516
Qad Noir <qadzealous@gmail.com>
2026-09-23 11:15:54 +0100

    Add printUsage function

--- bin/cli.dart
+++ bin/cli.dart
@@ -2,9 +2,16 @@ import 'package:cli/cli.dart' as cli;
 
 const version = '0.0.1';
 void main(List<String> arguments) {
-  if(arguments.isEmpty){
+  if (arguments.isEmpty) {
     print('Hello world!');
-  }else if(arguments.first == 'version'){
+  } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
   }
+}
+
+void printUsage() {
+  // Add this new function
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
+  );
 }
