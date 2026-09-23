ee6aaac24c954f8c4895e748bf1d28a8ad264182
Qad Noir <qadzealous@gmail.com>
2026-09-23 15:55:53 +0100

    Add optional metadata and usage getter

--- command_runner/lib/src/arguments.dart
+++ command_runner/lib/src/arguments.dart
@@ -1,8 +1,27 @@
 enum OptionType { flag, option }
 
 class Option {
-  Option(this.name, {required this.type});
+  Option(
+    this.name, {
+    required this.type,
+    this.help,
+    this.abbr,
+    this.defaultValue,
+    this.valueHelp,
+  });
 
   final String name;
   final OptionType type;
+  final String? help;
+  final String? abbr;
+  final Object? defaultValue;
+  final String? valueHelp;
+
+  String get usage {
+    if (abbr != null) {
+      return '-$abbr,--$name: $help';
+    }
+
+    return '--$name: $help';
+  }
 }
