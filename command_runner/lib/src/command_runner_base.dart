a4f0c78fe94c87f7445698e04ee33e7f6e9dfdeb
Qad Noir <qadzealous@gmail.com>
2026-09-25 12:56:00 +0100

    Add an onOutput callback

--- command_runner/lib/src/command_runner_base.dart
+++ command_runner/lib/src/command_runner_base.dart
@@ -6,12 +6,14 @@ import 'arguments.dart';
 import 'exceptions.dart';
 
 class CommandRunner {
-  CommandRunner({this.onError});
+  CommandRunner({this.onOutput, this.onError});
 
   final Map<String, Command> _commands = <String, Command>{};
 
   UnmodifiableSetView<Command> get commands =>
       UnmodifiableSetView<Command>(<Command>{..._commands.values});
+
+  FutureOr<void> Function(String)? onOutput;
 
   FutureOr<void> Function(Object)? onError;
 
