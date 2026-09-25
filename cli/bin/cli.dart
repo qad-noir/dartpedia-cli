0b56ce95d16763d8929bc8281a6e1d976594337a
Qad Noir <qadzealous@gmail.com>
2026-09-25 11:32:08 +0100

    Display extra logging information

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,11 +1,18 @@
 import 'package:cli/cli.dart' as cli;
+
 import 'dart:io';
+
 import 'package:http/http.dart' as http;
 import 'package:command_runner/command_runner.dart';
 
+void main() {
+  final verboseOption = Option(
+    'verbose',
+    type: OptionType.flag,
+    abbr: 'v',
+    help: 'Display extra logging information.',
+  );
 
-const version = '0.0.1';
-void main(List<String> arguments) async{
-  var runner = CommandRunner();
-  await runner.run(arguments);
+  print('Defined option: ${verboseOption.name}');
+  print('Usage: ${verboseOption.usage}');
 }
