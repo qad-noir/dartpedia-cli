ce403f48a57941549b6c17c9325b91254ff69e4d
Qad Noir <qadzealous@gmail.com>
2026-09-25 11:00:16 +0100

    Define abstract class and Option override

--- command_runner/lib/src/arguments.dart
+++ command_runner/lib/src/arguments.dart
@@ -1,6 +1,16 @@
 enum OptionType { flag, option }
 
-class Option {
+abstract class CliElement {
+  String get name;
+  String? get help;
+
+  Object? get defaultValue;
+  String? get valueHelp;
+
+  String get usage;
+}
+
+class Option extends CliElement {
   Option(
     this.name, {
     required this.type,
@@ -10,13 +20,19 @@ class Option {
     this.valueHelp,
   });
 
+  @override
   final String name;
   final OptionType type;
+  @override
   final String? help;
+  @override
   final String? abbr;
+  @override
   final Object? defaultValue;
+  @override
   final String? valueHelp;
 
+  @override
   String get usage {
     if (abbr != null) {
       return '-$abbr,--$name: $help';
