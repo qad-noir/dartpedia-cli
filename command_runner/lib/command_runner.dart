928329397c6dea7710adffdfc7f4048706f19d42
Qad Noir <qadzealous@gmail.com>
2026-09-25 11:19:40 +0100

    Update library with help command

--- command_runner/lib/command_runner.dart
+++ command_runner/lib/command_runner.dart
@@ -5,5 +5,6 @@ library;
 
 export 'src/command_runner_base.dart';
 export 'src/arguments.dart';
+export 'src/help_command.dart';
 
 // TODO: Export any libraries intended for clients of this package.
