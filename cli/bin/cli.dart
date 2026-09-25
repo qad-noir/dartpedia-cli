f31e851f4a7e4ccbac184302789cdceb7ab5f953
Qad Noir <qadzealous@gmail.com>
2026-09-25 11:35:52 +0100

    Connect CommandRunner and HelpCommand in the executable entry point.

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,18 +1,8 @@
-import 'package:cli/cli.dart' as cli;
-
-import 'dart:io';
-
-import 'package:http/http.dart' as http;
 import 'package:command_runner/command_runner.dart';
 
-void main() {
-  final verboseOption = Option(
-    'verbose',
-    type: OptionType.flag,
-    abbr: 'v',
-    help: 'Display extra logging information.',
-  );
+const version = '0.0.1';
 
-  print('Defined option: ${verboseOption.name}');
-  print('Usage: ${verboseOption.usage}');
+void main(List<String> arguments) {
+  var commandRunner = CommandRunner()..addCommand(HelpCommand());
+  commandRunner.run(arguments);
 }
